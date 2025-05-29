fb8c75df9d5cdf1ae4e7f875d3aaeaf6d721a140
Ahmed3zooz <ahmedazooz15@gmail.com>
2025-05-29 11:03:15 +0300

    home fragment design with adapter

---
+++ app/src/main/java/com/example/movieapp/ui/home/MovieAdapter.kt
@@ -0,0 +1,78 @@
+package com.example.movieapp.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.movieapp.R
+import com.example.movieapp.data.model.Movie
+import com.example.movieapp.databinding.ItemMovieBinding
+
+class MovieAdapter(
+    private val onMovieClick: (Movie) -> Unit,
+    private val onFavoriteClick: (Movie) -> Unit
+) : ListAdapter<Movie, MovieAdapter.MovieViewHolder>(MovieDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding = ItemMovieBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class MovieViewHolder(
+        private val binding: ItemMovieBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        init {
+            binding.root.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    onMovieClick(getItem(position))
+                }
+            }
+
+            binding.favoriteButton.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    onFavoriteClick(getItem(position))
+                }
+            }
+        }
+
+        fun bind(movie: Movie) {
+            binding.titleText.text = movie.title
+            binding.releaseDateText.text = movie.releaseDate
+            binding.favoriteButton.setImageResource(
+                if (movie.isFavorite) R.drawable.ic_favorite_filled
+                else R.drawable.ic_favorite_border
+            )
+
+            movie.posterPath?.let { posterPath ->
+                val imageUrl = "https://image.tmdb.org/t/p/w500$posterPath"
+                Glide.with(binding.root)
+                    .load(imageUrl)
+                    .centerCrop()
+                    .into(binding.posterImage)
+            }
+        }
+    }
+
+    private class MovieDiffCallback : DiffUtil.ItemCallback<Movie>() {
+        override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+            return oldItem == newItem
+        }
+    }
+} 
