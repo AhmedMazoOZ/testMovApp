dac4210af7a5e53eca0f453f989eacc0cd1a6c83
Ahmed3zooz <ahmedazooz15@gmail.com>
2025-05-31 10:42:00 +0300

    add get movie usecase unit test

--- domain/src/test/java/com/example/domain/ExampleUnitTest.kt
+++ domain/src/test/java/com/example/domain/ExampleUnitTest.kt
@@ -1,8 +1,18 @@
 package com.example.domain
 
+import com.example.domain.model.Movie
+import com.example.domain.repository.MovieRepository
+import com.example.domain.usecase.GetPopularMoviesUseCase
 import org.junit.Test
-
+import org.junit.Before
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.whenever
 import org.junit.Assert.*
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.test.runTest
+import org.mockito.Mockito.verify
+import org.hamcrest.MatcherAssert.assertThat
+import org.mockito.kotlin.whenever
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +23,30 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+}
+
+class GetMoviesUseCaseTest {
+    private lateinit var useCase: GetPopularMoviesUseCase
+    private lateinit var repository: MovieRepository
+    
+    @Before
+    fun setup() {
+        repository = mock()
+        useCase = GetPopularMoviesUseCase(repository)
+    }
+    
+    @Test
+    fun `when execute is called, then return movies from repository`() = runTest {
+        // Given
+        val movies = listOf(Movie(1, "Test Movie","","","","",5.0,"true))
+        whenever(repository.getMovies()).thenReturn(flow { emit(movies) })
+        
+        // When
+        val result = useCase().first()
+        
+        // Then
+        assertThat(result).isEqualTo(movies)
+        verify(repository).getMovies()
     }
 }
